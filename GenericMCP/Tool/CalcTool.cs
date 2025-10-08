d20aa7407bc5f350f311b275bead6383f81de707
Ravindran Keshavan <ravindran.k@gmail.com>
2025-10-08 14:28:01 +0530

    Add a tool to add 2 numbers

---
+++ GenericMCP/Tool/CalcTool.cs
@@ -0,0 +1,21 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace GenericMCP.Tool;
+
+/// <summary>
+/// Provides a tool to calculate.
+/// </summary>
+[McpServerToolType]
+public class CalcTool
+{
+    /// <summary>
+    /// Adds two numbers and sends back to the client.
+    /// </summary>
+    /// <param name="num1">Number1.</param>
+    /// <param name="num2">Number1.</param>
+    /// <returns>Sum of two numbers.</returns>
+    [McpServerTool, Description("Sends Sum back to the client.")]
+    public static int AddNumbers(int num1, int num2) => num1+num2;
+
+}
